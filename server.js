3e7eafa537280bb0097ac827669e99e83467561c
gabrielposumah <gjposumah@gmail.com>
2023-03-15 12:26:14 +0800

    routes & res

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//parse application
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//panggil router
+var routes = require('./routes');
+routes(app);
+
+app.listen(3000, () => {
+    console.log(`Server started on port`);
+});
